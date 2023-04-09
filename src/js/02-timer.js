5b35fabb3cea2a3932947bc4663b0fd386f6049b
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-09 08:08:04 +0300

    new

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+//======================================================
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  button: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+refs.button.disabled = true;
+refs.button.addEventListener('click', timerStart);
+let targetTime = null;
