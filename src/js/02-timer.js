700613b7c0e6516244af2d1cbb7dd69c7a06eabb
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-09 08:11:48 +0300

    new

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -53,3 +53,28 @@ function timerStart() {
     refs.seconds.textContent = `${seconds}`
   }, 1000);
 }
+//===========================================================
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = ddLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = ddLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = ddLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = ddLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+  
+    return { days, hours, minutes, seconds };
+  }
+  //===========================================================
+  function ddLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
