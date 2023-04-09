7881a5f75ba12230ebf34f6bfca5b445de140cd1
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-09 08:16:42 +0300

    new

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -21,3 +21,15 @@ refs.form.addEventListener('submit', e => {
           stepNum += Number(step.value)
   }
 })
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          if (shouldResolve) {
+              resolve({ position, delay })
+          }
+          reject({ position, delay });
+      }, delay)
+  })
+}
