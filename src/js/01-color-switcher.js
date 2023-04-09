2e03f0305e2ba4328ada669440bd2efa92ec824b
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-09 07:56:17 +0300

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -14,3 +14,20 @@ function getRandomHexColor() {
     makeColor.stop();
   });
   refs.buttonStop.disabled = true;
+
+  const makeColor = {
+    intervalId: null,
+    start() {
+      refs.buttonStop.disabled = false;
+      refs.buttonStart.disabled = true;
+      this.intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.intervalId);
+      refs.buttonStart.disabled = false;
+      refs.buttonStop.disabled = true;
+    },
+  };
+  
