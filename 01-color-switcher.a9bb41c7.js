58f6f379c33190e124d5448e0c8772c84f2d51ae
AnatoliiShkurpela <117720992+AnatoliiShkurpela@users.noreply.github.com>
2023-04-09 04:53:32 +0000

    Deploying to gh-pages from @ AnatoliiShkurpela/goit-js-hw-09@54b2ca16661b733726b7e27a42acc385b676b25a 🚀

---
+++ 01-color-switcher.a9bb41c7.js
@@ -0,0 +1,2 @@
+const t={buttonStart:document.querySelector("[data-start]"),buttonStop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.buttonStart.addEventListener("click",(()=>{makeColor.start()})),t.buttonStop.addEventListener("click",(()=>{makeColor.stop()})),t.buttonStop.disabled=!0;
+//# sourceMappingURL=01-color-switcher.a9bb41c7.js.map
