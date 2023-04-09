71ae0216136e67af233023e4bde0b454c41dd917
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-09 08:14:06 +0300

    new

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = { 
+  form: document.querySelector(".form")
 }
+console.dir(refs.form);
+refs.form.addEventListener('submit', e => {
+  e.preventDefault()
+  const { delay, step, amount } = e.currentTarget;
+  const delayNum = Number(delay.value)
+  let stepNum = delayNum
+  const amountNum = Number(amount.value)
+  for (let i = 1; i <= amountNum; i += 1) {
+
+      createPromise(i, stepNum)
+          .then(({ position, delay }) => {
+              Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+              Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+          stepNum += Number(step.value)
+  }
+})
